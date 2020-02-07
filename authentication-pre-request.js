32b9f19e92fa84b61e96df005a7f8da889877a63
Alex Ruef <alex.ruef@oregonstate.edu>
2020-02-07 15:13:35 -0800

    fixed unnecessary nested if

--- authentication-pre-request.js
+++ authentication-pre-request.js
@@ -10,26 +10,24 @@ const oauth2Request = {
 if (pm.environment.name === "LOCAL") {
     const token = btoa(pm.collectionVariables.get('username') + ":" + pm.collectionVariables.get('password'));
     pm.request.headers.add(new Header("Authorization: Basic " + token));
+} else if (pm.collectionVariables.get('bearerToken')
+            && pm.collectionVariables.get('tokenExpirationDate')
+            && pm.collectionVariables.get('tokenExpirationDate') > new Date().getTime()) {
+    pm.request.headers.add(new Header("Authorization: Bearer " + pm.collectionVariables.get('bearerToken')));
 } else {
-    if (pm.collectionVariables.get('bearerToken')
-          && pm.collectionVariables.get('tokenExpirationDate')
-          && pm.collectionVariables.get('tokenExpirationDate') > new Date().getTime()) {
-        pm.request.headers.add(new Header("Authorization: Bearer " + pm.collectionVariables.get('bearerToken')));
-    } else {
-        console.log("Getting new bearer token");
-        pm.sendRequest(oauth2Request, function (err, res) {
-            if (!err) {
-                const response = res.json();
-                const currDate = new Date();
-                currDate.setSeconds(currDate.getSeconds() + parseInt(response.expires_in));
-                pm.collectionVariables.set('bearerToken', response.access_token);
-                pm.collectionVariables.set('tokenExpirationDate', currDate.getTime());
-                pm.request.headers.add(new Header("Authorization: Bearer " + pm.collectionVariables.get('bearerToken')));
-            } else {
-                console.log("Could not retrieve oauth2 token");
-                console.log(err);
-                throw Error;
-            }
-        });
-    }
+    console.log("Getting new bearer token");
+    pm.sendRequest(oauth2Request, function (err, res) {
+        if (!err) {
+            const response = res.json();
+            const currDate = new Date();
+            currDate.setSeconds(currDate.getSeconds() + parseInt(response.expires_in));
+            pm.collectionVariables.set('bearerToken', response.access_token);
+            pm.collectionVariables.set('tokenExpirationDate', currDate.getTime());
+            pm.request.headers.add(new Header("Authorization: Bearer " + pm.collectionVariables.get('bearerToken')));
+        } else {
+            console.log("Could not retrieve oauth2 token");
+            console.log(err);
+            throw Error;
+        }
+    });
 }
