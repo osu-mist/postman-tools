07158c12c91ea8891eace9a717326cf5f54146f9
Alex Ruef <alex.ruef@oregonstate.edu>
2020-02-11 15:36:49 -0800

    more destructuring

--- authentication-pre-request.js
+++ authentication-pre-request.js
@@ -1,6 +1,9 @@
 const { Header } = require('postman-collection');
 
-const { collectionVariables } = pm;
+const { collectionVariables, request: { headers } } = pm;
+const bearerToken = collectionVariables.get('bearerToken');
+const tokenExpirationDate = collectionVariables.get('tokenExpirationDate');
+const { environment: environmentName } = pm.environment.name;
 
 const oauth2Request = {
   url: 'https://api.oregonstate.edu/oauth2/token',
@@ -11,14 +14,12 @@ const oauth2Request = {
       &client_secret=${collectionVariables.get('clientSecret')}`
 };
 
-const bearerToken = collectionVariables.get('bearerToken');
-const tokenExpirationDate = collectionVariables.get('tokenExpirationDate');
-if (pm.environment.name === 'LOCAL') {
+if (environmentName === 'LOCAL') {
   const token = btoa(
     `${collectionVariables.get('username')}:${collectionVariables.get('password')}`);
-  pm.request.headers.add(new Header(`Authorization: Basic ${token}`));
+  headers.add(new Header(`Authorization: Basic ${token}`));
 } else if (bearerToken && tokenExpirationDate && tokenExpirationDate > new Date().getTime()) {
-  pm.request.headers.add(new Header(`Authorization: Bearer ${bearerToken}`));
+  headers.add(new Header(`Authorization: Bearer ${bearerToken}`));
 } else {
   console.log('Getting new bearer token');
   pm.sendRequest(oauth2Request, (err, res) => {
@@ -29,7 +30,7 @@ if (pm.environment.name === 'LOCAL') {
       currDate.setSeconds(currDate.getSeconds() + parseInt(response.expires_in));
       collectionVariables.set('bearerToken', newToken);
       collectionVariables.set('tokenExpirationDate', currDate.getTime());
-      pm.request.headers.add(new Header(`Authorization: Bearer ${newToken}`));
+      headers.add(new Header(`Authorization: Bearer ${newToken}`));
     } else {
       console.log('Could not retrieve oauth2 token');
       if (err) console.log(err);
