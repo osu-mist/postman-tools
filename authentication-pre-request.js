2ceb20d4a99ebc613fb14424595669969f26aa14
Alex Ruef <alex.ruef@oregonstate.edu>
2020-02-12 09:36:48 -0800

    miscellaneous code improvements

--- authentication-pre-request.js
+++ authentication-pre-request.js
@@ -1,14 +1,19 @@
 const { Header } = require('postman-collection');
 const { stringify } = require('querystring');
 
-const { collectionVariables, request: { headers }, } = pm;
+const {
+  collectionVariables,
+  request: { headers },
+  environment: environmentVariables,
+  sendRequest
+} = pm;
 const bearerToken = collectionVariables.get('bearerToken');
 const tokenExpirationDate = collectionVariables.get('tokenExpirationDate');
 const username = collectionVariables.get('username');
 const password = collectionVariables.get('password');
-const authType = pm.environment.get('authType');
+const authType = environmentVariables.get('authType');
 
-const oauth2Request = {
+const oAuth2Request = {
   url: 'https://api.oregonstate.edu/oauth2/token',
   method: 'POST',
   header: 'Content-Type:application/x-www-form-urlencoded',
@@ -20,26 +25,29 @@ const oauth2Request = {
 };
 
 if (authType === 'basic') {
-  const token = btoa(
-    `${username}:${password}`);
+  const token = btoa(`${username}:${password}`);
   headers.add(new Header(`Authorization: Basic ${token}`));
-} else if (bearerToken && tokenExpirationDate && tokenExpirationDate > new Date().getTime()) {
-  headers.add(new Header(`Authorization: Bearer ${bearerToken}`));
+} else if (authType === 'bearer') {
+  if (bearerToken && tokenExpirationDate && tokenExpirationDate > new Date().getTime()) {
+    headers.add(new Header(`Authorization: Bearer ${bearerToken}`));
+  } else {
+    console.log('Getting new bearer token');
+    sendRequest(oAuth2Request, (err, res) => {
+      if (!err && res.code === 200) {
+        const response = res.json();
+        const newToken = response.access_token;
+        const currDate = new Date();
+        currDate.setSeconds(currDate.getSeconds() + parseInt(response.expires_in));
+        collectionVariables.set('bearerToken', newToken);
+        collectionVariables.set('tokenExpirationDate', currDate.getTime());
+        headers.add(new Header(`Authorization: Bearer ${newToken}`));
+      } else {
+        console.log('Could not retrieve OAuth2 token');
+        if (err) console.log(err);
+        throw new Error(`${res.code}: ${res.status}`);
+      }
+    });
+  }
 } else {
-  console.log('Getting new bearer token');
-  pm.sendRequest(oauth2Request, (err, res) => {
-    if (!err && res.code === 200) {
-      const response = res.json();
-      const newToken = response.access_token;
-      const currDate = new Date();
-      currDate.setSeconds(currDate.getSeconds() + parseInt(response.expires_in));
-      collectionVariables.set('bearerToken', newToken);
-      collectionVariables.set('tokenExpirationDate', currDate.getTime());
-      headers.add(new Header(`Authorization: Bearer ${newToken}`));
-    } else {
-      console.log('Could not retrieve oauth2 token');
-      if (err) console.log(err);
-      throw new Error(`${res.code}: ${res.status}`);
-    }
-  });
+  throw new Error(`Unrecognized authType: ${authType}. Valid authTypes are 'bearer' or 'basic'`);
 }
